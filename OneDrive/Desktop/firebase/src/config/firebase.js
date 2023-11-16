213a8ac209edb0ff2434cb4a5d89ea9cc627c439
Hafeefa <hafeefapc2003@gmail.com>
2023-11-17 03:24:28 +0530

    latest

--- OneDrive/Desktop/firebase/src/config/firebase.js
+++ OneDrive/Desktop/firebase/src/config/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB5-XgeY8lAinxClDPbsfaxpv3g3TpROqs",
@@ -8,9 +10,11 @@ const firebaseConfig = {
   storageBucket: "fir-course-2c133.appspot.com",
   messagingSenderId: "859789562005",
   appId: "1:859789562005:web:f1ba95a83dfa2e57b0cb9c",
-   measurementId: "G-KBR2Y32GYB"
+  measurementId: "G-KBR2Y32GYB"
 };
 
-
 const app = initializeApp(firebaseConfig);
-export const auth =getAuth(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
