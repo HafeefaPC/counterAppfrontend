3c8e685a8b7ff0b7bda841ebc0e0a989b32f1109
Hafeefa <hafeefapc2003@gmail.com>
2024-02-11 00:51:43 +0530

    schema added

--- OneDrive/Desktop/ecommerce-sanity-nextjs/components/product/[id]/page.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const productpage = ({ id }) => {
-  return <div></div>;
-};
--- OneDrive/Desktop/ecommerce-sanity-nextjs/sanity/schema.js
+++
@@ -1,7 +0,0 @@
-
-import {product} from './schemas/product'
-import {webview} from './schemas/webview'
-
-export const schema = {
-  types: [product,webview],
-}
