4c96337620f8499fd43aaa0783b3d7ea9d3c14b8
Hafeefa <hafeefapc2003@gmail.com>
2024-02-17 20:40:15 +0530

    page rounting went correctly

--- OneDrive/Desktop/ecommerce-sanity-nextjs/app/product/[productId]/page.jsx
+++ OneDrive/Desktop/ecommerce-sanity-nextjs/app/product/[productId]/page.jsx
@@ -3,38 +3,37 @@ import Navbar from "../../../common/navbar";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import client from "../../../sanity/lib/client";
+import { urlForImage } from "../../../sanity/lib/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function produtdetails({ params }) {
-  const productId = params;
-
-  const [product, setProduct] = useState(null);
+  const productId = params.productId; // Access the specific parameter 'productId'
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    if (!productId) return;
-
     const groqQuery = `
-        *[_type == "product" && _id == $productId] {
-          id,
-          title,
-          description,
-          images[],
-          price,
-          rating
-        }[0]
-      `;
-
-    const params = { productId };
+      *[_type == "product"] {
+        id,
+        title,
+        description,
+        images[],
+        price,
+        rating
+      }
+    `;
 
     client
-      .fetch(groqQuery, params)
-      .then((product) => {
-        console.log("Fetched product:", product);
-        setProduct(product);
+      .fetch(groqQuery)
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setData(data);
       })
       .catch((error) => {
-        console.error("Error fetching product:", error);
+        console.error("Error fetching data:", error);
       });
-  }, [productId]);
+  }, []);
 
   const renderRatingStars = (rating) => {
     const stars = [];
@@ -50,39 +49,45 @@ export default function produtdetails({ params }) {
 
   return (
     <>
-      <div className="flex p-4 gap-5">
-        {product && (
-          <div
-            key={product.id}
-            className="border border-gray-300 rounded-2xl w-1/4 relative"
-          >
-            <div className=" gap-4 p-10">
-              {product.images.map((image, index) => (
-                <div key={index}>
-                  {index === 0 && (
-                    <Image
-                      src={urlForImage(image)}
-                      width={200}
-                      height={200}
-                      alt={`Product Image ${index}`}
-                    />
-                  )}
+      <Navbar />
+      <div className="grid grid-cols-5 items-center">
+        {data.map(
+          (product) =>
+            productId.toString() === product.id.toString() && (
+              <div
+                key={product.id}
+                className="border border-gray-300 rounded-2xl w-full relative col-start-2 col-span-3 bg-black"
+              >
+                <div className=" gap-4 p-10 items-center  justify-center    ">
+                  <Slider>
+                    {product.images.map((image, index) => (
+                      <div key={index} className="w-1/2 items-center">
+                        <Image
+                          src={urlForImage(image)}
+                          width={400}
+                          height={400}
+                          alt={`Product Image ${index}`}
+                          className=" object-cover w-full "
+                        />
+                      </div>
+                    ))}
+                  </Slider>
                 </div>
-              ))}
 
-              <div className="absolute bottom-0 left-0 w-full bg-white p-3">
-                <button className="bg-black text-white p-2 w-full">
-                  Add to Cart
-                </button>
-                <h2 className=" font-bold">{product.title}</h2>
-                {/* <p>{product.description}</p> */}
-                <p className="text-[#FF0000]">₹{product.price}</p>
-                <div className="flex items-center">
-                  {renderRatingStars(product.rating)}
+                <div className="absolute bottom-0 left-0 w-full bg-white p-3">
+                  <button className="bg-black text-white p-2 w-full">
+                    Add to Cart
+                  </button>
+                  <h2 className="font-bold">{product.title}</h2>
+
+                  <p className="text-[#FF0000]">₹{product.price}</p>
+                  <p>{product.description}</p>
+                  <div className="flex items-center">
+                    {renderRatingStars(product.rating)}
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            )
         )}
       </div>
     </>
