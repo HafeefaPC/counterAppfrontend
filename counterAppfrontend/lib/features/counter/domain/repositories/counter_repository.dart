ccb2490801708893658b188ab158b69600939826
HafeefaPC <121932603+HafeefaPC@users.noreply.github.com>
2024-06-30 01:51:59 +0530

    Update counter_repository.dart

--- counterAppfrontend/lib/features/counter/domain/repositories/counter_repository.dart
+++ counterAppfrontend/lib/features/counter/domain/repositories/counter_repository.dart
@@ -1,14 +1,15 @@
-import 'package:counterapp/features/counter/data/models/get_limit_model.dart';
 import 'package:dio/dio.dart';
-
+import '../../data/models/get_limit_model.dart';
 import '../../data/models/limit_res_model.dart';
 import '../../data/models/update_limit_model.dart';
 
 
-
 class CounterRepository {
-  final Dio _dio = Dio();
-  final String baseUrl = "http://127.0.0.1:8000"; // Adjust the IP address for your environment
+  final Dio _dio;
+  final String baseUrl;
+
+  CounterRepository({required Dio dio, required this.baseUrl})
+      : _dio = dio;
 
   Future<GetLimitModel> fetchLimit() async {
     try {
@@ -29,23 +30,23 @@ class CounterRepository {
   }
 
   Future<LimitResModel> setLimit(UpdateLimitModel updateLimitModel) async {
-  try {
-    print("Sending PUT request to: $baseUrl/counter/");
-    final response = await _dio.put(
-      '$baseUrl/counter/',
-      data: {"limit_value": updateLimitModel.maxLimit}, // Ensure the correct JSON structure
-    );
-    print("Response Status Code: ${response.statusCode}");
-    print("Response Data: ${response.data}");
+    try {
+      print("Sending PUT request to: $baseUrl/counter/");
+      final response = await _dio.put(
+        '$baseUrl/counter/',
+        data: {"limit_value": updateLimitModel.maxLimit}, // Ensure the correct JSON structure
+      );
+      print("Response Status Code: ${response.statusCode}");
+      print("Response Data: ${response.data}");
 
-    if (response.statusCode == 200) {
-      return LimitResModel.fromJson(response.data);
-    } else {
+      if (response.statusCode == 200) {
+        return LimitResModel.fromJson(response.data);
+      } else {
+        throw Exception('Failed to update limit');
+      }
+    } catch (e) {
+      print("Error occurred while updating limit: $e");
       throw Exception('Failed to update limit');
     }
-  } catch (e) {
-    print("Error occurred while updating limit: $e");
-    throw Exception('Failed to update limit');
   }
-}
 }
