ab07538f42ab891a33138bb9de64d7928351f41e
HafeefaPC <121932603+HafeefaPC@users.noreply.github.com>
2024-06-30 01:51:22 +0530

    Update main.dart

--- counterAppfrontend/lib/main.dart
+++ counterAppfrontend/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/dio.dart';
 
 import 'features/counter/domain/repositories/counter_repository.dart';
-import 'features/counter/domain/services/get_limit.dart';
-import 'features/counter/domain/services/set_limit.dart';
 import 'features/counter/presentation/bloc/counter_bloc.dart';
 import 'features/counter/presentation/pages/counter_page.dart';
 
@@ -15,20 +13,28 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Dio dio = Dio(); // Create Dio instance
-
-    final GetLimit getLimit = GetLimit(dio, baseUrl: "http://127.0.0.1:8000");
-    final SetLimit setLimit = SetLimit(dio, baseUrl: "http://127.0.0.1:8000");
+    final Dio dio = Dio();
+    final String baseUrl = "http://127.0.0.1:8000";
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Counter App',
       theme: ThemeData(
-       
+        primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (context) => CounterBloc(getLimit: getLimit, setLimit: setLimit),
-        child: CounterPage(),
+      home: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<CounterRepository>(
+            create: (context) => CounterRepository(dio: dio, baseUrl: baseUrl),
+          ),
+ 
+        ],
+        child: BlocProvider(
+          create: (context) => CounterBloc(
+            counterRepository: RepositoryProvider.of<CounterRepository>(context),
+          ),
+          child: CounterPage(),
+        ),
       ),
     );
   }
